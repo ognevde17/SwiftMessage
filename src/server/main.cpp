c0e2688a4f22bebf02ab2276d2554fbb7ae63e2e
Viktor Perepechaev <viktor2309msk@gmail.com>
2025-03-06 00:41:53 +0300

    created basic boost program

---
+++ src/server/main.cpp
@@ -0,0 +1,12 @@
+#include <algorithm>
+#include <iostream>
+#include <iterator>
+
+#include <boost/lambda/lambda.hpp>
+
+int main() {
+  using namespace boost::lambda;
+  typedef std::istream_iterator<int> in;
+
+  std::for_each(in(std::cin), in(), std::cout << (_1 * 3) << " ");
+}
