281b23438dda16d5f9254a92395526bffd3b7be5
Viktor Perepechaev <viktor2309msk@gmail.com>
2025-03-06 15:07:08 +0300

    added better README.md + created example program

--- src/server/main.cpp
+++ src/server/main.cpp
@@ -1,12 +1,48 @@
-#include <algorithm>
+//
+// blocking_udp_echo_server.cpp
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//
+// Copyright (c) 2003-2024 Christopher M. Kohlhoff (chris at kohlhoff dot com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+
+#include <boost/asio/ts/buffer.hpp>
+#include <boost/asio/ts/internet.hpp>
+#include <cstdlib>
 #include <iostream>
-#include <iterator>
 
-#include <boost/lambda/lambda.hpp>
+using boost::asio::ip::udp;
 
-int main() {
-  using namespace boost::lambda;
-  typedef std::istream_iterator<int> in;
+enum { max_length = 1024 };
 
-  std::for_each(in(std::cin), in(), std::cout << (_1 * 3) << " ");
+void server(boost::asio::io_context& io_context, unsigned short port) {
+  udp::socket sock(io_context, udp::endpoint(udp::v4(), port));
+  for (;;) {
+    char data[max_length];
+    udp::endpoint sender_endpoint;
+    size_t length = sock.receive_from(boost::asio::buffer(data, max_length),
+                                      sender_endpoint);
+    std::cout << "Я принял сообщение: ";
+    std::cout.write(data, length) << '\n';
+    sock.send_to(boost::asio::buffer(data, length), sender_endpoint);
+  }
+}
+
+int main(int argc, char* argv[]) {
+  try {
+    if (argc != 2) {
+      std::cerr << "Usage: blocking_udp_echo_server <port>\n";
+      return 1;
+    }
+
+    boost::asio::io_context io_context;
+
+    server(io_context, std::atoi(argv[1]));
+  } catch (std::exception& e) {
+    std::cerr << "Exception: " << e.what() << "\n";
+  }
+
+  return 0;
 }
