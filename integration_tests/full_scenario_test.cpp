ca38ec794a8ad472371a6d53308df7ae4e67b01b
Viktor Perepechaev <viktor2309msk@gmail.com>
2025-04-29 17:46:51 +0300

    added GoogleTests into pipeline

--- integration_tests/full_scenario_test.cpp
+++ integration_tests/full_scenario_test.cpp
@@ -1,22 +1,65 @@
 #include <gtest/gtest.h>
-#include <thread>
-#include <chrono>
+
 #include <atomic>
-#include <memory>
+#include <chrono>
 #include <exception>
 #include <iostream>
+#include <memory>
 #include <string>
+#include <thread>
 
-TEST(FullScenarioTest, FullScenario) {
-    EXPECT_TRUE(true);
-}
+#include "../include/client/client.hpp"
+#include "../include/common/messages.hpp"
+#include "../include/server/server.hpp"
 
-TEST(FullScenarioTest2, FullScenario) {
-    EXPECT_TRUE(2 == 2);
-}
+class ServerThread {
+ public:
+  ServerThread() : running_(false) {}
+
+  void Start() {
+    if (running_.load()) return;
+
+    running_.store(true);
+    thread_ = std::thread([this] {
+      try {
+        Server::GetInstance().Run();
+      } catch (const std::exception& ex) {
+        std::cerr << "[ServerThread] exception: " << ex.what() << std::endl;
+      }
+    });
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+  }
+
+  void Stop() {
+    if (!running_.load()) return;
+
+    running_.store(false);
+    Server::GetInstance().Stop();
+
+    if (thread_.joinable()) thread_.join();
+  }
+
+  ~ServerThread() { Stop(); }
+
+ private:
+  std::thread thread_;
+  std::atomic<bool> running_;
+};
+
+// TEST(ServerTest, InitializeServer) {
+//   ServerThread server_thread;
+//   server_thread.Start();
+
+//   std::this_thread::sleep_for(std::chrono::seconds(100));
+
+//   server_thread.Stop();
+// }
+
+TEST(FullScenarioTest2, FullScenario) { EXPECT_TRUE(2 == 2); }
 
 // Точка входа для GTest
-int main(int argc, char **argv) {
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
+int main(int argc, char** argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
